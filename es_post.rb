95e49c1b5302aca4859fa1d8dc3c07011ea7268b
maxZimm <mkz.87.draw@gmail.com>
2020-04-02 13:42:58 -0600

    remove stale reference to item from list_view

--- es_post.rb
+++ es_post.rb
@@ -1,14 +1,24 @@
-
+require 'selenium-webdriver'
+require 'pry'
 
 class Post
   def initialize( driver, element)
-    @driver = driver
-    @element = element
-    puts post_type( @element )
-    scroll_to( @element )
+      #@driver = Selenium::WebDriver.for :chrome
+      @driver = driver
+      @element = element
+      @time 
+      post_action( @element )
   end
   # TODO if it is a media element click play
-  # TODO make wait time based on content
+  # TODO make wait time based on content | DONE
+  # TODO fix missing context when driver is assinged to default content
+attr_reader :type, :ended, :driver
+
+  def post_action( post )
+    post_type( post )
+    scroll_to( post )
+    sleep_time( post )
+  end
 
   def post_type(post)
     begin
@@ -17,21 +27,33 @@ class Post
     rescue Selenium::WebDriver::Error::NoSuchElementError 
       @type = :image
     end 
+    puts "#{ post.ref } | #{ post.location.y }"
     @type
   end
 
   def scroll_to(post)
       @driver.execute_script( "arguments[0].scrollIntoView( { 'behavior': 'smooth',
       'block': 'center'})", post)
-      sleep( 6 )
   end
 
   def gif_length( post )
     l1 = post.find_element( css: 'iframe')
     @driver.switch_to.frame( l1 )
-    l2 = post.find_element( css: 'iframe')
-    @driver.switch_to.frame( l1 )
-    l3 = post.find_element( css: 'iframe')
-    l3.find_element( css: 'progress-control')
+    l2 = @driver.find_element( css: 'iframe')
+    @driver.switch_to.frame( l2 )
+    l3 = @driver.find_element( css: 'iframe')
+    @driver.switch_to.frame( l3 )
+    time = @driver.find_element( css: '.progress-control')
+    @time = time.attribute('data-tooltip').to_i
+  end
+
+  def sleep_time( post )
+    if @type == :gif
+      sleep( gif_length( post ))
+    else
+      @time = 4
+      sleep(@time)
+    end
+    puts @type, @time
   end
 end
--- es_v3.rb
+++ es_v3.rb
@@ -15,13 +15,17 @@ class EndlessScroll
   end
 
 
-  def endless_scroll(list)
-    begin
+  def endless_scroll( list )
+    # We have 2 cases for what needs to be consumed by update list
+    # 1 it's an image and the driver hasn't changed so we proceed as normal, list_view iterates to next step of each loop
+    # 2 it'a a gif and we break out of each loop which then sends back into endless_scroll method
+    # however the local variable list is still assigned to the origial @list with stale elements
+    # this is not good. 
+    # TODO identify/confirm that this @list is the problem
+    # TODO break apart the process of moving throuh the list view
+    # TODO enable gif types to be processed completely 
+    # TODO list_view will continue from where it left off after previous gif
       list_view(list)
-    ensure
-      list[-1] != nil
-    end
-      binding.pry
       newer = update_list(list)
       endless_scroll(newer)
   end
@@ -39,20 +43,30 @@ class EndlessScroll
 
   def list_view(list)
     list.each do |item|
-      Post.new( @driver, item)
-      puts "#{item.ref}  : #{item.location.y}"
-      # TODO if it is a media element click play
-      # TODO make wait time based on content
-      #sleep(5)
+      post =  Post.new( @driver, item)
+      if post.type == :gif
+        #@driver.switch_to.default_content maybe do this within update
+        break
+      end
     end
   end
 
   def update_list(list)
+    stale_check( list )
     update = @driver.find_elements(:css, ".Post")
-    cut_point = update.find_index {|el| el.ref == list[-1].ref }
-    cut_point = cut_point + 1
-    out = update.slice( cut_point..-1 )
-    out
+    if !list.empty?
+      cut_point = update.find_index {|el| el.ref == list[-1].ref }
+      cut_point = cut_point + 1
+      out = update.slice( cut_point..-1 )
+      return out
+    else
+      return update
+    end
+  end
+
+  def stale_check( item )
+    puts item.class
+    binding.pry
   end
 end
 
