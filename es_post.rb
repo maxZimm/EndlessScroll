8e810c5324f985ceac216449d41f68b1731c91b4
maxZimm <mkz.87.draw@gmail.com>
2020-04-06 18:14:09 -0500

    GOT GIFS TO PLAY WITH SEND KEYS

--- es_post.rb
+++ es_post.rb
@@ -9,9 +9,6 @@ class Post
       @time 
       post_action( @element )
   end
-  # TODO if it is a media element click play
-  # TODO make wait time based on content | DONE
-  # TODO fix missing context when driver is assinged to default content
 attr_reader :type, :ref, :driver
 
   def post_action( post )
@@ -43,13 +40,19 @@ attr_reader :type, :ref, :driver
     @driver.switch_to.frame( l2 )
     l3 = @driver.find_element( css: 'iframe')
     @driver.switch_to.frame( l3 )
-    time = @driver.find_element( css: '.progress-control')
-    @time = time.attribute('data-tooltip').to_i
+    begin
+      vid = @driver.find_element(css: '.video.media')
+      vid.send_keys( :space )
+		  time = @driver.find_element( css: '.progress-control')
+      @time = time.attribute('data-tooltip').to_i
+    rescue Selenium::WebDriver::Error::NoSuchElementError 
+      @time = 5
+    end
   end
 
   def sleep_time( post )
     if @type == :gif
-      sleep( gif_length( post ) * 1.25)
+      sleep( gif_length( post ) * 1.2)
     else
       @time = 5
       sleep(@time)
